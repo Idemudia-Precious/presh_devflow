c3e7392b052691283aed79d285bfe962f57b06bc
Idemudia-Precious <idemudiapreciousosaze@gmail.com>
2024-10-26 23:45:47 +0100

    ESLint & Prettier Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-
 const Home = () => {
   return (
-    <h1 className="text-3xl text-violet-700">Welcome to the world of NextJS</h1>
-  )
-}
+    <h1 className="text-3xl text-violet-700 font-black">
+      Welcome to the world of NextJS
+    </h1>
+  );
+};
 
-export default Home
+export default Home;

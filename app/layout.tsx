c1e991bfa9a5c3be3a170bd80e3f5242f26b675c
Idemudia-Precious <idemudiapreciousosaze@gmail.com>
2024-10-28 06:02:22 +0100

    assets and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,19 +14,23 @@ const geistMono = localFont({
   weight: "100 900",
 });
 const inter = localFont({
-  src: "./fonts/inter.woff",
+  src: "./fonts/interVF.ttf",
   variable: "--font-space-grotesk",
   weight: "100 200 300 400 500 700 700 900",
 });
 const spaceGrotesk = localFont({
-  src: "./fonts/SpaceGrotesk.woff",
+  src: "./fonts/SpaceGroteskVF.ttf",
   variable: "--font-space-grotesk",
   weight: "300 400 500 700 700 900",
 });
 
 export const metadata: Metadata = {
   title: "DevFlow",
-  description: "A better version of StackOverFlow",
+  description:
+    "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+  icons: {
+    icon: "/images/site-logo.svg",
+  },
 };
 
 export default function RootLayout({
@@ -37,7 +41,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${inter.className} ${spaceGrotesk.variable} antialiased`}
+        className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
       >
         {children}
       </body>

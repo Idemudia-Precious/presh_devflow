3e18b2e30d1aae0502f14bc83d42674881ec3609
Idemudia-Precious <idemudiapreciousosaze@gmail.com>
2024-11-01 10:58:07 +0100

    Implement QuestionEditor

---
+++ components/editor/index.tsx
@@ -0,0 +1,121 @@
+"use client";
+// InitializedMDXEditor.tsx
+import type { ForwardedRef } from "react";
+import {
+  headingsPlugin,
+  listsPlugin,
+  quotePlugin,
+  thematicBreakPlugin,
+  markdownShortcutPlugin,
+  MDXEditor,
+  type MDXEditorMethods,
+  toolbarPlugin,
+  ConditionalContents,
+  ChangeCodeMirrorLanguage,
+  UndoRedo,
+  Separator,
+  BoldItalicUnderlineToggles,
+  ListsToggle,
+  CreateLink,
+  InsertImage,
+  InsertTable,
+  InsertThematicBreak,
+  InsertCodeBlock,
+  linkPlugin,
+  linkDialogPlugin,
+  tablePlugin,
+  imagePlugin,
+  codeBlockPlugin,
+  codeMirrorPlugin,
+  diffSourcePlugin,
+} from "@mdxeditor/editor";
+import "@mdxeditor/editor/style.css";
+import { basicDark } from "cm6-theme-basic-dark";
+import "./dark-editor.css";
+import { useTheme } from "next-themes";
+
+interface Props {
+  value: string;
+  fieldChange: (value: string) => void;
+  editorRef: ForwardedRef<MDXEditorMethods> | null;
+}
+
+const Editor = ({ value, editorRef, fieldChange, ...props }: Props) => {
+  const { resolvedTheme } = useTheme();
+
+  const theme = resolvedTheme === "dark" ? [basicDark] : [];
+
+  return (
+    <MDXEditor
+      key={resolvedTheme}
+      markdown={value}
+      className="background-light800_dark200 light-border-2 markdown-editor dark-editor w-full border"
+      onChange={fieldChange}
+      plugins={[
+        headingsPlugin(),
+        listsPlugin(),
+        linkPlugin(),
+        linkDialogPlugin(),
+        quotePlugin(),
+        markdownShortcutPlugin(),
+        tablePlugin(),
+        imagePlugin(),
+        codeBlockPlugin({ defaultCodeBlockLanguage: "" }),
+        codeMirrorPlugin({
+          codeBlockLanguages: {
+            css: "css",
+            txt: "txt",
+            sql: "sql",
+            html: "html",
+            sass: "sass",
+            scss: "scss",
+            bash: "bash",
+            json: "json",
+            js: "javascript",
+            ts: "typescript",
+            "": "unspecified",
+            tsx: "TypeScript (React)",
+            jsx: "JavaScript (React)",
+          },
+          autoLoadLanguageSupport: true,
+          codeMirrorExtensions: theme,
+        }),
+        diffSourcePlugin({ viewMode: "rich-text", diffMarkdown: "" }),
+        toolbarPlugin({
+          toolbarContents: () => (
+            <ConditionalContents
+              options={[
+                {
+                  when: (editor) => editor?.editorType === "codeblock",
+                  contents: () => <ChangeCodeMirrorLanguage />,
+                },
+                {
+                  fallback: () => (
+                    <>
+                      <UndoRedo />
+                      <Separator />
+                      <BoldItalicUnderlineToggles />
+                      <Separator />
+                      <ListsToggle />
+                      <Separator />
+                      <CreateLink />
+                      <InsertImage />
+                      <Separator />
+                      <InsertTable />
+                      <InsertThematicBreak />
+                      <InsertCodeBlock />
+                    </>
+                  ),
+                },
+              ]}
+            />
+          ),
+        }),
+      ]}
+      {...props}
+      ref={editorRef}
+    />
+  );
+};
+
+export default Editor;
--- components/forms/QuestionForm.tsx
+++ components/forms/QuestionForm.tsx
@@ -2,7 +2,7 @@
 
 import { AskQuestionSchema } from "@/lib/validations";
 import { zodResolver } from "@hookform/resolvers/zod";
-import React from "react";
+import React, { useRef } from "react";
 import { Path, useForm } from "react-hook-form";
 import {
   Form,
@@ -13,11 +13,18 @@ import {
   FormLabel,
   FormMessage,
 } from "../ui/form";
-import { render } from "react-dom";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
+import { MDXEditorMethods } from "@mdxeditor/editor";
+import dynamic from "next/dynamic";
+
+const Editor = dynamic(() => import("@/components/editor"), {
+  // Make sure we turn SSR off
+  ssr: false,
+});
 
 const QuestionForm = () => {
+  const editorRef = useRef<MDXEditorMethods>(null);
   const form = useForm({
     resolver: zodResolver(AskQuestionSchema),
     defaultValues: {
@@ -66,9 +73,10 @@ const QuestionForm = () => {
                 <span className="text-primary-500">*</span>
               </FormLabel>
               <FormControl>
-                <Input
-                  {...field}
-                  className="paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 no-focus min-h-[56px] border"
+                <Editor
+                  value={field.value}
+                  fieldChange={field.onChange}
+                  editorRef={editorRef}
                 />
               </FormControl>
               <FormDescription className="body-regular text-light-500 mt-2.5">
